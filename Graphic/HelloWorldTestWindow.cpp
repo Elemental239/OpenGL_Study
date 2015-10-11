f3ae8aafa096226e116e0b699271474b629799ed
Andrey Piatygin <elemental239@rambler.ru>
2015-10-12 00:50:04 +0300

    Draw simple black screen

--- Graphic/HelloWorldTestWindow.cpp
+++ Graphic/HelloWorldTestWindow.cpp
@@ -1,4 +1,4 @@
-// Possible OpenGL tutorials: http://www.learnopengl.com/#!Getting-started/Creating-a-window
+// Possible OpenGL tutorials: http://www.learnopengl.com/#!Getting-started/Creating-a-window  - now it is the main course
 //							  https://open.gl/
 // OpenGL main site links:    https://www.opengl.org/sdk/docs/tutorials/
 
@@ -29,9 +29,41 @@ void CHelloWorldTestWindow::Draw()
 {
 	MARKER("CHelloWorldTestWindow::Draw()");
 
+	//Init GLFW
 	glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	//Create window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		LOGE("Failed to create GLFW window");
+		glfwTerminate();
+		return;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Init GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		LOGE("Failed to initialize GLEW");
+		return;
+	}
+
+	//Set ViewPort
+	glViewport(0, 0, 800, 600);  
+
+	//Start event loop
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	// End of all
+	glfwTerminate();
 }
