619e8fff3c899a7d0333aa473bc032d9b72a6f56
andrey.piatygin <elemental239@rambler.ru>
2015-10-23 01:04:07 +0300

    CString -> CStringWide function

--- Utility/StringImpl.h
+++ Utility/StringImpl.h
@@ -2,7 +2,20 @@
 #define __Utility_String_H__
 
 #include <string>
+#include <windows.h>
 
 #define CString std::string
+#define CStringWide std::wstring
+
+CStringWide CString2CStringWide(const CString string)
+{
+	CStringWide wideString(string.begin(), string.end());
+	return wideString;
+}
+
+LPCWSTR CString2LPCWSTR(const CString string)
+{
+	return (LPCWSTR)CString2CStringWide(string).c_str();
+}
 
 #endif //__Utility_String_H__
