fef70f1ee7d1a4fd2cc343df56c07bb4006460c2
Andrey Piatygin <elemental239@rambler.ru>
2015-11-04 02:04:46 +0300

    Introduce ApplicationInfo.h

---
+++ Logic/ApplicationInfo.h
@@ -0,0 +1,9 @@
+#ifndef __Logic_ApplicationInfo_H__
+#define __Logic_ApplicationInfo_H__
+
+static int MAJOR_APPLICATION_VERSION = 0;
+static int MIDDLE_APPLICATION_VERSION = 0;
+static int MINOR_APPLICATION_VERSION = 1;
+#define MAIN_WINDOW_LABEL "El OpenGL"
+
+#endif //__Logic_ApplicationInfo_H__
