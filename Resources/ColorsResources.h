b6a67aa86ab1615d534c72ac2e46624749d211d3
Andrey Piatygin <elemental239@rambler.ru>
2015-11-14 14:52:39 +0300

    Introduce ColorResources.h for often used colors

---
+++ Resources/ColorsResources.h
@@ -0,0 +1,12 @@
+#ifndef __Resources_ColorsResources_H__
+#define __Resources_ColorsResources_H__
+
+#include "GraphicGeneral.h"
+
+static const CColor COLOR_WHITE		("FFFFFF");
+static const CColor COLOR_BLACK		("000000");
+static const CColor COLOR_RED		("FF0000");
+static const CColor COLOR_GREEN		("00FF00");
+static const CColor COLOR_BLUE		("0000FF");
+
+#endif //__Resources_ColorsResources_H__
