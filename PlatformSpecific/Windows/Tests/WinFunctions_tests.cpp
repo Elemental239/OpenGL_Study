1569b6e5f3889eeffc903706f7e5c181b209fa78
Andrey Piatygin <elemental239@rambler.ru>
2015-11-21 15:21:50 +0300

    String to path tests => better function

--- PlatformSpecific/Windows/Tests/WinFunctions_tests.cpp
+++ PlatformSpecific/Windows/Tests/WinFunctions_tests.cpp
@@ -1,9 +1,77 @@
-static int FailFunc()
-{
-	return 0;
-}
+#include "Catch\include\catch.hpp"
+#include "Windows/WinFunctions.h"
 
-static int SuccessFunc()
+TEST_CASE("ConvertStringToPath() test", "[Windows][String][Path]")
 {
-	return 1;
+	SECTION("Nothing to do")
+	{
+		CString path = "\\input.txt";
+		CString output = ConvertStringToPath(path);
+
+		REQUIRE(path == output);
+	}
+
+	SECTION("All is ok")
+	{
+		CString path = "\\\"my documents\"\\input.txt";
+		CString output = ConvertStringToPath(path);
+
+		REQUIRE(path == output);
+	}
+
+	SECTION("Just change / to \\")
+	{
+		CString path = "/documents/input.txt";
+		CString rightOutput = "\\documents\\input.txt";
+		CString output = ConvertStringToPath(path);
+
+		REQUIRE(rightOutput == output);
+	}
+
+	SECTION("Just escape path with space")
+	{
+		CString path = "\\my documents\\input.txt";
+		CString rightOutput = "\\\"my documents\"\\input.txt";
+		CString output = ConvertStringToPath(path);
+
+		REQUIRE(rightOutput == output);
+	}
+
+	SECTION("escape space & change /")
+	{
+		CString path = "/my documents/kvaka_dom/input.txt";
+		CString rightOutput = "\\\"my documents\"\\kvaka_dom\\input.txt";
+		CString output = ConvertStringToPath(path);
+
+		REQUIRE(rightOutput == output);
+	}
+
+	SECTION("String with no start slash")
+	{
+		CString path = "input.txt";
+		CString rightOutput = "\\input.txt";
+		CString output = ConvertStringToPath(path);
+
+		REQUIRE(rightOutput == output);
+	}
+
+	SECTION("Wrong arg: empty arg")
+	{
+		REQUIRE(ConvertStringToPath("") == CString(""));
+	}
+
+	SECTION("Wrong arg: / input")
+	{
+		REQUIRE(ConvertStringToPath("/") == CString(""));
+		REQUIRE(ConvertStringToPath("\\") == CString(""));
+	}
+
+	SECTION("Arg with / at the end")
+	{
+		CString path = "/documents/input.txt/";
+		CString rightOutput = "\\documents\\input.txt";
+		CString output = ConvertStringToPath(path);
+
+		REQUIRE(rightOutput == output);
+	}
 }
--- PlatformSpecific/Windows/WinFunctions.cpp
+++ PlatformSpecific/Windows/WinFunctions.cpp
@@ -11,12 +11,35 @@ LPCWSTR CString2LPCWSTR(const CString string)
 
 CString ConvertStringToPath(CString input)
 {
+	if (input.length() < 1)
+	{
+		LOGE("ConvertStringToPath::empty input");
+		return "";
+	}
+
 	//at first, convert all '/' with "\\"
-	size_t nPos = input.find("/");
+	size_t nPos = input.find('/');
 	while (nPos != std::string::npos)
 	{
 		input.replace(nPos, 1, "\\");
-		nPos = input.find("/", nPos + 1);
+		nPos = input.find('/', nPos + 1);
+	}
+
+	// Check starting slash presence, ending slash unpresence, length
+	if (input[0] != '\\')
+		input.insert(0, "\\");
+
+	int nIter = input.length() - 1;
+	while (nIter >= 0 && input[nIter] == '\\')
+	{
+		input.erase(nIter, 1);
+		nIter -= 1;
+	}
+
+	if (input.length() < 1)
+	{
+		LOGE("ConvertStringToPath::empty string");
+		return "";
 	}
 
 	//at second, surround all "<word> <word>" with \"
@@ -32,10 +55,13 @@ CString ConvertStringToPath(CString input)
 			return input;
 		}
 
-		input.insert(leftPos, "\"");
-		input.insert(rightPos - 1, "\"");
+		if (input.substr(rightPos - 1, 1) != "\"")
+			input.insert(rightPos, "\"");
 
-		nPos = input.find(" ");
+		if (input.substr(leftPos + 1, 1) != "\"")
+			input.insert(leftPos + 1, "\"");
+
+		nPos = input.find(" ", nPos + 2);
 	}
 
 	return input;
