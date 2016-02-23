0619f43114c30b48d9b8e1d76281ce2786870aef
Andrey Piatygin <elemental239@rambler.ru>
2016-02-23 13:48:13 +0300

    Add enum class value -> int function

--- Utility/InlineFunctions.h
+++ Utility/InlineFunctions.h
@@ -1,8 +1,16 @@
 #ifndef __Utility_InlineFunctions_H__
 #define __Utility_InlineFunctions_H__
 
+#include <type_traits> //for std::underlying_type
+
 #define ABS(a) (((a) > 0) ? (a) : (-(a)))
 #define MAX2(a, b) ((a) > (b) ? (a) : (b))
 #define MAX3(a, b, c) (MAX2(MAX2((a), (b)), (c)))
+
+template<typename E>
+/*constexpr El239: - can be, but not in VS2012*/ auto enumValueToInt(E e) -> typename std::underlying_type<E>::type // Convert any enum class value to underlying int type
+{
+   return static_cast<typename std::underlying_type<E>::type>(e);
+}
 
 #endif //__Utility_InlineFunctions_H__
