185a5a765c75afcb2c820bd0f3fe279973b11df6
Andrey Piatygin <elemental239@rambler.ru>
2016-04-24 22:06:30 +0300

    Stub for ConveyLifeGameDialog

---
+++ Graphic/Dialogs/ConveyLifeGameDialog.h
@@ -0,0 +1,14 @@
+#ifndef __Graphic_ConveyLifeGameDialog_H__
+#define __Graphic_ConveyLifeGameDialog_H__
+
+#include "Dialog.h"
+
+class CConveyLifeGameDialog : public CDialog
+{
+public:
+	CConveyLifeGameDialog();
+
+private:
+};
+
+#endif //__Graphic_ConveyLifeGameDialog_H__
