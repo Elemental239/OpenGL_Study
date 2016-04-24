2b92fd5d2919547de109868ae08f9a584cf0d554
Andrey Piatygin <elemental239@rambler.ru>
2016-04-25 01:41:22 +0300

    Stub for Grid control for Convey's game of life

---
+++ Graphic/Controls/Grid.cpp
@@ -0,0 +1,7 @@
+#include "Grid.h"
+#include "Logger.h"
+
+CGridControl::CGridControl()
+{
+	MARKER("CGridControl::CGridControl()");
+}
---
+++ Graphic/Controls/Grid.h
@@ -0,0 +1,12 @@
+#ifndef __Graphic_Grid_H__
+#define __Graphic_Grid_H__
+
+#include "Controls/Control.h"
+
+class CGridControl : public CControl
+{
+public:
+	CGridControl();
+};
+
+#endif //__Graphic_Grid_H__
