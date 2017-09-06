74c9d267ff238d9ac67d4dcf088ff06301725b35
Andrei <Piatygin>
2017-09-06 11:36:15 +0300

    Minor: make enumValueToInt constrexp cause it's allowed in VS2015

--- Utility/InlineFunctions.h
+++ Utility/InlineFunctions.h
@@ -8,7 +8,7 @@
 #define MAX3(a, b, c) (MAX2(MAX2((a), (b)), (c)))
 
 template<typename E>
-/*constexpr El239: - can be, but not in VS2012*/ auto enumValueToInt(E e) -> typename std::underlying_type<E>::type // Convert any enum class value to underlying int type
+constexpr auto enumValueToInt(E e) -> typename std::underlying_type<E>::type // Convert any enum class value to underlying int type
 {
    return static_cast<typename std::underlying_type<E>::type>(e);
 }
